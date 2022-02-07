c770c801905e585899d81fa25386bda3301b255c
doyunnn <73818291+doyunnn@users.noreply.github.com>
2022-02-07 15:25:45 +0900

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxMemo
+//
+//  Created by do_yun on 2022/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
