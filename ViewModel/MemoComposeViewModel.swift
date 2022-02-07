003295b9ddd33b9f714c9863a178bb0f12aebd6d
doyunnn <73818291+doyunnn@users.noreply.github.com>
2022-02-07 21:45:58 +0900

    [FEAT] 메모 생성 기능 구현

--- ViewModel/MemoComposeViewModel.swift
+++ ViewModel/MemoComposeViewModel.swift
@@ -1,4 +1,3 @@
-//
 //  MemoComposeViewMoel.swift
 //  RxMemo
 //
@@ -8,6 +7,38 @@
 import Foundation
 import RxSwift
 import RxCocoa
-class MemoComposeViewModel{
+import Action
+
+class MemoComposeViewModel : CommonViewModel{
+    private let content : String?
     
+    var initialText : Driver<String?>{
+        return Observable.just(content).asDriver(onErrorJustReturn: nil)
+    }
+    
+    let saveAction : Action<String, Void>
+    let cancelAction : CocoaAction
+    
+    init(title:String, content: String? = nil, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType, saveAction: Action<String, Void>? = nil, cancelAction : CocoaAction? = nil){
+        self.content = content
+        
+        self.saveAction = Action<String, Void>{ input in
+            if let action = saveAction{
+                action.execute(input)
+            }
+            
+            return sceneCoordinator.close(animated: true).asObservable().map{ _ in }
+            
+        }
+        
+        self.cancelAction = CocoaAction{
+            if let action = cancelAction{
+                action.execute(())
+            }
+            
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+        }
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
