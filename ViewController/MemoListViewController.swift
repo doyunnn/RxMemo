d3a267ec7e01b0a7ac945240b0d1172aa4bdec77
doyunnn <73818291+doyunnn@users.noreply.github.com>
2022-02-07 16:36:53 +0900

    [ADD] 뷰컨 추가

---
+++ ViewController/MemoListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MemoListViewController.swift
+//  RxMemo
+//
+//  Created by do_yun on 2022/02/07.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
